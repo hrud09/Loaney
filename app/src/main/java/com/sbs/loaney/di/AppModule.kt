d76f5e596244e99299f9ee89a307060658acc3e5
Mahadi Hassan <65780684+hrud09@users.noreply.github.com>
2026-06-21 01:44:41 +0600

    Update_LTS

--- app/src/main/java/com/sbs/loaney/di/AppModule.kt
+++ app/src/main/java/com/sbs/loaney/di/AppModule.kt
@@ -3,7 +3,7 @@ package com.sbs.loaney.di
 import android.content.Context
 import com.sbs.loaney.data.local.AppDatabase
 import com.sbs.loaney.data.repository.ILoanRepository
-import com.sbs.loaney.data.repository.FirebaseLoanRepository
+import com.sbs.loaney.data.repository.LoanRepository
 import com.sbs.loaney.data.repository.SettingsRepository
 import com.sbs.loaney.data.repository.dataStore
 import dagger.Module
@@ -25,8 +25,11 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideLoanRepository(firebaseRepository: FirebaseLoanRepository): ILoanRepository {
-        return firebaseRepository
+    fun provideLoanRepository(database: AppDatabase): ILoanRepository {
+        return LoanRepository(
+            loanDao = database.loanDao(),
+            bankAccountDao = database.bankAccountDao()
+        )
     }
 
     @Provides
